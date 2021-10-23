ac0280faa6f4567aee083f1a83341fdd7d44b356
konmarii <chankonworld@gmail.com>
2021-10-23 17:50:03 +0900

    commit

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String word;
+		word = "麻里奈";
+		System.out.println(word);
+		
+		word = "Love the life you live. Live the life you love.";
+		System.out.println(word);
+		
+		final int LUCKY_NUMBER;
+		LUCKY_NUMBER = 3;
+		System.out.println(LUCKY_NUMBER);
+	}
+}
---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,31 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1234567890L;
+		System.out.println(long1);
+		
+		double double1 = 12345678.9d;
+		System.out.println(double1);
+		
+		boolean boolean1 = true;
+		System.out.println(boolean1);
+		
+		Float float1 = 0.123456789f;
+		System.out.println(float1);
+		
+		Integer integer1 = 123;
+		System.out.println(integer1);
+		
+		Character character1 = 'あ';
+		System.out.println(character1);
+		
+		int[] array1 = new int[5];
+		array1[0] = 00;
+		array1[1] = 10;
+		array1[2] = 20;
+		array1[3] = 30;
+		array1[4] = 40;
+		int int1 = array1[3];
+		System.out.println(int1);
+	}
+}
